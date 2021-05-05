8c1f9bcb40f852b623dccc938fa77fb77c83e68f
max mcruvie <mcruvie@hotmail.co.uk>
2021-05-05 09:39:15 +0100

    Content_Serialization_with_apiCode_commented

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -6,6 +6,8 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace NVS_Project.Controllers
 {
@@ -35,8 +37,22 @@ namespace NVS_Project.Controllers
         [Consumes("application/json")]
         public IActionResult PostUser(Models.userDetails userInfo)
         {
-            var result = userInfo; 
-            return Ok(result);
+            string url = "https://apitest.hedd.ac.uk:8443";
+
+            var serializedContents = JsonSerializer.Serialize(userInfo);
+
+            /*
+            using (var client = new WebClient())
+            {
+                client.Headers[HttpRequestHeader.ContentType] = "application/json";
+                //client.Headers["Authorization"] = "1DTLNLZ9PS2YLD0KOMKZ1DTLNLZ9PS2YLD0KOMK";
+                string s = client.UploadString(url, serializedContents);
+                Console.WriteLine(s);
+            }
+            */
+            return Ok(serializedContents);
+            //var result = userInfo; 
+            //return Ok(result);
         }
     }
 }
